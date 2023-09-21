f5e4fcb707c348bb1c5a3588b7d655690a216dfd
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-09-21 16:26:39 +0900

    Q3

--- main.c
+++ main.c
@@ -6,14 +6,12 @@
 int main(int argc, char *argv[]) 
 {
 	char c;
-	int i;
-	
-	printf("input a number : ");
+
+	printf("input a character : ");
 	scanf("%c", &c);
 	
-	i = c - '0';
-	printf("The input number ASCII is %i\n", c);
-	printf("The input number is %i\n", i);
+	printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+	
 	system ("PAUSE");
 	
 	return 0;
